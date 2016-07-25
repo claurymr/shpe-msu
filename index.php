94371776121be4529328beb7010bb17c7548f81c
mejiarod <mejiarod@msu.edu>
2016-07-25 13:10:18 -0400

    First commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sakura
+ * Date: 7/7/2016
+ * Time: 3:20 PM
+ */
+?>
+
+<!Doctype html>
+<html>
+<head>
+    <title>SHPE|MSU</title>
+</head>
+<body>
+    <nav></nav>
+    <div id="main">
+        
+    </div>
+</body>
+</html>
