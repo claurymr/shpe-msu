7d9522b653351c77d978e1ae9a839bfd08818512
mejiarod <mejiarod@msu.edu>
2016-08-06 18:26:31 -0400

    Added css for small devices and finished half section of index.

---
+++ header.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sakura
+ * Date: 7/31/2016
+ * Time: 5:25 PM
+ */
+?>
+
+<header>
+    <div class="inner-container">
+        <div class="master-head">
+            <a href="./"> <img src="assets/img/shpe-logo.svg"></a>
+            <div class="society-name">
+                <p>SHPE | Michigan State University</p>
+                <p>Society of Hispanic Professional Engineers</p>
+            </div>
+        </div>
+        <nav>
+            <a href="index.php" class="selected">Home</a>
+            <a href="">About Us</a>
+            <a href="">MemberSHPE</a>
+            <a href="">SponsorSHPE</a>
+            <a href="">Contact</a>
+        </nav>
+    </div>
+</header>
--- index.php
+++ index.php
@@ -14,33 +14,70 @@
     <link rel="stylesheet" type="text/css" href="css/normalize.css">
     <link rel="stylesheet" type="text/css" href="css/uikit.min.css">
     <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="css/media-queries.css">
 </head>
 <body>
     <div id="main">
-        <header>
-            <div class="inner-container">
-                <div class="master-head">
-                    
+        <?php include "header.php" ?>
+        <div id="banner"">
+            <div id="image-banner"></div>
+            <div id="mission-vision">
+                <div class="inner-container">
+                    <h2>Mission</h2>
+                    <p>SHPE changes lives by empowering the Hispanic community to
+                        realize its fullest potential and to impact the world through
+                        STEM awareness, access, support and development.</p>
+                    <h2>Vision</h2>
+                    <p>SHPE's vision is a world where Hispanics are highly valued and
+                        influential as the leading innovators, scientists, mathematicians
+                        and engineers.</p>
                 </div>
-                <nav></nav>
             </div>
-        </header>
-        <div id="mission-vision">
-
         </div>
         <div id="shpe-highlights">
-
+            <div class="inner-container">
+                <div class="announcements">
+                    <h2>Highlights</h2>
+                    <div class="uk-grid">
+                        <div class="uk-width-1-3">
+                            <div class="announcement">
+                                <p>ANNOUNCEMENT 1</p>
+                            </div>
+                        </div>
+                        <div class="uk-width-2-3">
+                            <div class="announcement">
+                                <p>ANNOUNCEMENT 2</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="uk-grid">
+                        <div class="uk-width-2-3">
+                            <div class="announcement">
+                                <p>ANNOUNCEMENT 3</p>
+                            </div>
+                        </div>
+                        <div class="uk-width-1-3">
+                            <div class="announcement">
+                                <p>ANNOUNCEMENT 4</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
         <div id="events">
+            <div class="inner-container uk-grid">
+                <div class="uk-width-1-3">
+                    <div id="upcoming-events">
 
-            <div id="upcoming-events">
+                    </div>
+                </div>
+                <div class="uk-width-2-3">
+                    <div id="event-of-week">
 
+                    </div>
+                </div>
             </div>
-
-            <div id="event-of-week">
-
-            </div>
-
         </div>
         <footer>
 
