a09525ea7a73baa42c5b758dbf04caaac6ea114b
Christian Narvios <narviosc@msu.edu>
2016-08-06 21:41:07 -0400

    Footer
    Images might still need some work since it does not display block the
    whole width once screen is minimized.

---
+++ Footer/footer.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<link href="footer_class.css" rel="stylesheet" type="text/css">
+<link href="Sponsors.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+</body>
+
+<footer>
+
+	<div id="Divsion">
+    
+            <h2> Sponsored by: </h2>
+            
+    </div>
+    
+    <div id="Sponsors">
+    	
+    	<a href="footer.php"><img src="https://christiannarvios.files.wordpress.com/2016/07/gm_foundation_logo_grey.png" class="GM"/></a>
+        
+        <a href="footer.php"><img src="https://christiannarvios.files.wordpress.com/2016/07/dte_energy.png" class="DTE"/></a>
+        
+        <a href="footer.php"><img src="https://christiannarvios.files.wordpress.com/2016/07/just-the-whirlpool-logo.png" class="Whirlpool"/></a>
+        <a href="footer.php"><img src="https://christiannarvios.files.wordpress.com/2016/07/jackson-national-life.png" class="Jackson"/></a>
+    
+    </div>
+     
+    	<div id="link_text">
+        
+            <div id="Column_1">
+                <h3>Join Now</h3>
+                <h3>Scholarships</h3>
+                <h3>Regional SHPE</h3>
+            </div>
+            
+            <div id="Column_2">
+                <h3>National SHPE</h3>
+                <h3>SHPE Foundation</h3>
+                <h3>SHPE Detroit</h3>
+            </div>
+            
+        </div>
+            
+        <div id="Social_Media">
+                     
+            <a href="index.html"><img src="https://christiannarvios.files.wordpress.com/2016/07/black_fb.png" onmouseover="this.src='https://christiannarvios.files.wordpress.com/2016/07/facebook-e1470009019796.png'" onmouseout="this.src='https://christiannarvios.files.wordpress.com/2016/07/black_fb.png'" alt="Facebook logo to our SHPE page" class="Facebook"/></a>
+            
+            <a href="index.html"><img src="https://christiannarvios.files.wordpress.com/2016/07/white_instagram.png" onmouseover="this.src='https://christiannarvios.files.wordpress.com/2016/07/instagram-logo1.png'" onmouseout="this.src='https://christiannarvios.files.wordpress.com/2016/07/white_instagram.png'" alt="Instagram link to our SHPE Page." class="Instagram"/></a>
+                  
+        </div>
+                      
+    
+        <div id="Authors">
+        
+        	<p>Created by: Claury Mejia | Christian Narvios </p> 
+        
+        </div>
+                    
+</footer>
+
+</html>
